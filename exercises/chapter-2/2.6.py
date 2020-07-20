36e3676210458a891123672cd44d18d037770cc6
samuelyapwoonpin <samuelywp@gmail.com>
2020-07-20 23:41:33 +0800

    chore: add chapter 2 exercises

---
+++ exercises/chapter-2/2.6.py
@@ -0,0 +1,11 @@
+"""
+2-6. Famous Quote 2: 
+Repeat Exercise 2-5, but this time store the famous person’s name in a variable
+called famous_person. Then compose your message and store it in a new variable 
+called message. Print your message.
+"""
+
+famous_person = "Srinivasa Ramanujan"
+message = "An equation for me has no meaning, unless it expresses a thought of God."
+print(famous_person + ' once said, "' + message + '"')
+
---
+++ exercises/chapter-2/2.9.py
@@ -0,0 +1,9 @@
+"""
+2-9. Favorite Number: 
+Store your favorite number in a variable. Then, using that variable, create a 
+message that reveals your favorite number. Print that message.
+"""
+
+number = 13
+message = "My favorite number is " + str(number) + "!"
+print(message)
